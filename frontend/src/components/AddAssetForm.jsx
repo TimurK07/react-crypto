be052c42057e73b03b13efefc43477defc30306a
Timur Kaliyev <timurkaliev115@gmail.com>
2024-02-02 19:21:28 +0600

    added my app

---
+++ frontend/src/components/AddAssetForm.jsx
@@ -0,0 +1,149 @@
+import { Select, Space, Divider, Form, Button, InputNumber, DatePicker, Result } from "antd";
+import { useRef, useState } from "react";
+import { useCrypto } from "../context/crypto-context";
+import CoinInfo from "./CoinInfo";
+
+const validateMessages = {
+  required: '${label} is required!',
+  types:{
+    number: '${label} is not valid number!',
+  },
+  number: {
+    range: '${label} must between ${min} and ${max}'
+  }
+}
+
+export default function AddAssetForm({ onClose }) {
+  const [form] = Form.useForm()
+  const { crypto, addAsset } = useCrypto();
+  const [coin, SetCoin] = useState(null);
+  const [ submitted, setSubmitted ] = useState(false)
+  const assetRef =  useRef()
+
+  if(submitted) {
+    return (
+      <Result
+        status="success"
+        title="New Asset Added!"
+        subTitle={`Addded ${assetRef.current.amount} of ${coin.name} by price ${assetRef.current.price}`}
+        extra={[
+          <Button type="primary" key="console" onClick={onClose}>
+            Close
+          </Button>
+        ]}
+      />
+    )
+  }
+
+  if (!coin) {
+    return (
+      <Select
+        style={{
+          width: "100%",
+        }}
+        onSelect={(v) => SetCoin(crypto.find((c) => c.id === v))}
+        placeholder="Select coin"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: 40 }}
+              src={option.data.icon}
+              atl={option.data.label}
+            />{" "}
+            {option.data.label}
+          </Space>
+        )}
+      />
+    );
+  }
+
+  function onFinish(values) {
+    const newAsset =  {
+      id: coin.id,
+      amount: values.amount,
+      price: values.price,
+      date: values.date?.$d ?? new Date() ,
+    }
+    assetRef.current = newAsset
+    setSubmitted(true)
+    addAsset(newAsset)
+  }
+
+  function handleAmountChange(value) {
+    const price = form.getFieldValue('price')
+    form.setFieldsValue({
+      total: +(value * price).toFixed(2),
+    })
+  }
+
+  function handlePriceChange(value) {
+    const amount = form.getFieldValue('amount')
+    form.setFieldsValue({
+      total: +(amount * value).toFixed(2),
+    })
+  }
+
+  return (
+    <Form
+      form={form}
+      name="basic"
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 10,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        price: +coin.price.toFixed((2)),
+      }}
+      onFinish={onFinish}
+      validateMessages={validateMessages}
+    >
+      <CoinInfo coin={coin}/>
+      <Divider />
+
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            type: 'number',
+            min: 0,
+          },
+        ]}
+      >
+        <InputNumber 
+          placeholder="Enter coin amount" 
+          onChange={handleAmountChange}
+          style={{width: '100%'}}/>
+      </Form.Item>
+
+      <Form.Item label="Price" name="price">
+        <InputNumber onChange={handlePriceChange} style={{width: '100%'}}/>
+      </Form.Item>
+
+      <Form.Item label="Date & Time" name="date">
+        <DatePicker showTime style={{width: '100%'}}/>
+      </Form.Item>
+
+      <Form.Item label="Total & Time" name="total">
+        <InputNumber disabled style={{width: '100%'}}/>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
---
+++ frontend/src/context/crypto-context.jsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { fakeFetchCrypto, fetchAssets } from "../api";
+import { percentDiference } from "../utils";
+
+
+const CryptoContext = createContext({
+    assets: [],
+    crypto: [],
+    loading: false,
+})
+
+
+export function CryptoContextProvider({ children }) {
+    const [loading, setLoading] = useState(false);
+    const [crypto, setCrypto] = useState([]);
+    const [assets, setAssets] = useState([]);
+
+    function mapAssets(assets,result) {
+        return assets.map(asset => {
+        const coin = result.find((c) => c.id === asset.id);
+        return {
+            grow: asset.price < coin.price,
+            growPrecent: percentDiference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            name: coin.name,
+            ...asset,
+        };
+        })
+        
+    }
+
+    useEffect(() => {
+        async function preload() {
+            setLoading(true);
+            const { result } = await fakeFetchCrypto();
+            const assets = await fetchAssets();
+
+            setAssets(mapAssets(assets, result));
+            setCrypto(result);
+            setLoading(false);
+        }
+        preload();
+    }, []);
+
+    function addAsset(newAsset) {
+        setAssets((prev) => mapAssets([...prev, newAsset], crypto))
+    }
+
+    return (
+        <CryptoContext.Provider value={{ loading, crypto, assets, addAsset }}>
+            {children}
+        </CryptoContext.Provider>
+    )
+}
+export default CryptoContext
+
+export function useCrypto() {
+    return useContext(CryptoContext)
+}
